05f94709706824a93a5da6037da21a83d74f7e1e
babas <banasti@gmail.com>
2022-08-15 14:27:25 +0700

    menambahkan kode menampilkan data customer pada controller

--- controller.js
+++ controller.js
@@ -7,3 +7,15 @@ var connection = require('./koneksi');
 exports.index = function (req,res){
     response.ok("Aplikasi REST API ku berjalan",res)
 };
+
+//menampilkan semua data costumer
+
+exports.tampildatapelanggan = function (req, res) {
+    connection.query('SELECT * FROM dim_customer', function (error, rows, fields){
+        if(error){
+            console.log(error);
+        }else{
+            response.ok(rows, res)
+        }
+    });
+};
